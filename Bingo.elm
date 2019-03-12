dc7a52f19d6d07000ac9b1a2ca48f8d912623814
Mayank Gandhi <mayank.gandhi@gemsessence.com>
2019-03-13 00:07:35 +0530

    Functions done

--- Bingo.elm
+++ Bingo.elm
@@ -3,5 +3,17 @@ module Bingo exposing (..)
 
 import Html
 
+-- main =
+--   Html.text (String.repeat 3 (String.toUpper "Elm live working"))
+
+{-
 main =
-  Html.text "Elm live working with div just like react!!!"
+  "- Elm live working - " |> String.toUpper |> String.repeat 3 |> Html.text
+-}
+
+main =
+  "Elm live working"
+    |> String.toUpper
+    |> String.repeat 3
+    |> String.pad 100 '*'
+    |> Html.text
